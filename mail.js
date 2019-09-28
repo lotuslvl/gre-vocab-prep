68544c6bca622dd84fae99bf2180215bebe65a6d
Alexandra Skinner <aleksandrayandovka@gmail.com>
2019-09-27 22:52:57 -0500

    added comments for email.js

--- mail.js
+++ mail.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 // here is our main package for sending emails.
 //  in this case it sends and gets emails only for gmail 
 const nodemailer = require('nodemailer');
+var userEmail;
 
 // step 1 - we create a transport
 let transporter = nodemailer.createTransport({
@@ -18,7 +19,7 @@ let transporter = nodemailer.createTransport({
 // step 2 - mail template
 let mailText = {
     from: process.env.EMAIL,
-    to: 'lotuslindez@gmail.com',
+    to: userEmail,
     subject: 'I works!',
     text: 'Here is my text...',
     html: '<h2> <bold> You result is here<bold> </h2>' 
@@ -30,6 +31,7 @@ transporter.sendMail(mailText, function (err, data) {
     } else {
 
         console.log("it was sent");
-    }
-})
+      }
+    })
+
 // ps: you also need to turn on less security app in gmail, I turned it on for our account
