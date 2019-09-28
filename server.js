8af4ee5ebfe8366876dd1239ad54f1475b7a3cfd
LotusGit <lotuslindez@gmail.com>
2019-09-27 22:34:25 -0500

    added question functionality, adjusted model for text completion

--- server.js
+++ server.js
@@ -25,14 +25,14 @@ app.use(express.static("public"));
 
 // Routes
 // =============================================================
-//require("./routes/api-routes.js")(app);
+require("./routes/api-routes.js")(app);
 require("./routes/html-routes.js")(app);
 //require("./routes/admin-routes.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync({force:true}).then(function() {
+db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+    console.log("App listening on PORT http://localhost:8080/");
   });
 });
