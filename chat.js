2331be80516e295c25d393a60a39a94009249787
Alexandra Skinner <aleksandrayandovka@gmail.com>
2019-09-30 23:16:52 -0500

    added 2 chat files to run it independently and added chat.js to server js and chat.html to a scoreboard.html

--- chat.js
+++ chat.js
@@ -6,11 +6,15 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/chat.html');
 });
 
-io.on('connection', function(socket){
-    socket.on('chat message', function(msg){
-      io.emit('chat message', msg);
+io.on('connection', function (socket) {
+    socket.on('chat message', function (msg) {
+
+        io.sockets.emit('chat message', msg);
     });
-  });
+    socket.on('typing', function (data) {
+        socket.broadcast.emit('typing', data);
+    })
+});
 http.listen(3000, function () {
     console.log('listening on *:3000');
 });
