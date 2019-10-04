ee82deb3dd12b0e8a11d37b7d0720b8bccda4463
LotusGit <lotuslindez@gmail.com>
2019-10-03 20:42:36 -0500

    style changes and database changes

--- models/readingcomprehensionq.js
+++ models/readingcomprehensionq.js
@@ -1,29 +1,18 @@
 module.exports = function(sequelize, DataTypes) {
-    var ReadingComprehensionQ = sequelize.define("TextCompletionQ", {
+    var ReadingComprehensionQ = sequelize.define("ReadingComprehensionQ", {
 
       difficulty: DataTypes.INTEGER,
 
       passage: DataTypes.TEXT,
       question1: DataTypes.STRING,
-      correctanswer1: DataTypes.STRING,
+      correctanswer: DataTypes.STRING,
       wronganswerA1: DataTypes.STRING,
       wronganswerB1: DataTypes.STRING,
       wronganswerC1: DataTypes.STRING,
       wronganswerD1: DataTypes.STRING,
+      wronganswerE1: DataTypes.STRING,
 
-      question2: DataTypes.TEXT,
-      correctanswer2: DataTypes.STRING,
-      wronganswerA2: DataTypes.STRING,
-      wronganswerB2: DataTypes.STRING,
-      wronganswerC2: DataTypes.STRING,
-      wronganswerD2: DataTypes.STRING,
 
-      question3: DataTypes.TEXT,
-      correctanswer3: DataTypes.STRING,
-      wronganswerA3: DataTypes.STRING,
-      wronganswerB3: DataTypes.STRING,
-      wronganswerC3: DataTypes.STRING,
-      wronganswerD3: DataTypes.STRING,  
     });
     return ReadingComprehensionQ;
   };
